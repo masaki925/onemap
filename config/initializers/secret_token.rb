b1d33ce0d5a3d538e35ff1bcba805e123effdb2d
masaki925 <masaki925@gmail.com>
2013-04-29 13:38:46 +0900

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onemap::Application.config.secret_token = '8e28ebb5a221cb8c80b7f66b8d94233339606e473d179c6d52d14f573d1538781149d40cdabb934a51265714c0c1ba008e2b25a8dbacae4944081b630669aee6'
