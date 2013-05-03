604a46c12484b003b175f93ae97f5e917e6f9e76
masaki925 <masaki925@gmail.com>
2013-05-03 14:58:12 +0900

    spot のカラムをlocalis に合わせたかんじ

--- db/migrate/20130502232105_create_spots.rb
+++ db/migrate/20130502232105_create_spots.rb
@@ -2,11 +2,18 @@ class CreateSpots < ActiveRecord::Migration
   def change
     create_table :spots do |t|
       t.string :name, null:false
+      t.string :google_reference
       t.string :address
       t.string :tel
       t.string :station
       t.integer :take_time
       t.float :cost
+      t.string :lat
+      t.string :lng
+      t.string :site_url
+      t.string :image_url
+      t.text   :description
+      t.string :station
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -51,14 +51,20 @@ ActiveRecord::Schema.define(:version => 20130503003608) do
   end
 
   create_table "spots", :force => true do |t|
-    t.string   "name",       :null => false
+    t.string   "name",             :null => false
+    t.string   "google_reference"
     t.string   "address"
     t.string   "tel"
     t.string   "station"
     t.integer  "take_time"
     t.float    "cost"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "lat"
+    t.string   "lng"
+    t.string   "site_url"
+    t.string   "image_url"
+    t.text     "description"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
   end
 
   create_table "users", :force => true do |t|
